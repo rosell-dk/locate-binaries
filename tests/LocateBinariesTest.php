53ad04c174d3ed356a9ad626d05c42ea299de0c8
rosell-dk <bjorn@rosell.dk>
2022-04-19 13:50:06 +0200

    more testing

--- tests/LocateBinariesTest.php
+++ tests/LocateBinariesTest.php
@@ -7,22 +7,24 @@ use LocateBinaries\LocateBinaries;
 
 class LocateBinariesTest extends TestCase
 {
+
     public function testLocateInCommonSystemPaths()
     {
         $binaries = LocateBinaries::locateInCommonSystemPaths('ls');
         $this->assertGreaterThanOrEqual(1, count($binaries));
     }
 
-    public function locateBinariesUsingWhereIs()
+    public function testLocateBinariesUsingWhereIs()
     {
         $whereIsBinaries = LocateBinaries::locateInCommonSystemPaths('whereis');
+        //$this->assertGreaterThanOrEqual(1, count($whereIsBinaries));
         if (count($whereIsBinaries) > 0) {
             $binaries = LocateBinaries::locateBinariesUsingWhereIs('ls');
             $this->assertGreaterThanOrEqual(1, count($binaries));
         }
     }
 
-    public function locateBinariesUsingWhich()
+    public function testLocateBinariesUsingWhich()
     {
         $whichBinaries = LocateBinaries::locateInCommonSystemPaths('which');
         if (count($whichBinaries) > 0) {
@@ -30,4 +32,28 @@ class LocateBinariesTest extends TestCase
             $this->assertGreaterThanOrEqual(1, count($binaries));
         }
     }
+
+    public function testLocateInstalledBinaries()
+    {
+        $whichBinaries = LocateBinaries::locateInCommonSystemPaths('which');
+        if (count($whichBinaries) > 0) {
+            $binaries = LocateBinaries::locateInstalledBinaries('ls');
+            $this->assertGreaterThanOrEqual(1, count($binaries));
+        }
+    }
+
+    public function testLocateInstalledNoBinariesFound()
+    {
+        $binaries = LocateBinaries::locateInstalledBinaries('lsbananaflip');
+        $this->assertEquals(0, count($binaries));
+    }
+/*
+    public function testLocateInstalledNoExecAvail()
+    {
+        // If no exec() is avail, we expect Exception... But how do we simulate that exec() is unavailable?
+        // $this->expectException(\Exception::class);
+        // how to test this?
+    }*/
+
+
 }
